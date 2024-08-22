7ab27a28572c8ca566ff1c6ae024b36cc2e876f2
blackDesertShinBeolUi <yanghye0321@naver.com>
2024-08-22 16:04:29 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './img/file.png';
+import ReactPlayer from 'react-player'; // ReactPlayer 가져오기
+import video from './video/test1.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo">
+          <img src={logo} alt="HYOSUNG INC Logo" />
+        </div>
+        <nav className="navbar">
+          <ul className="nav-links">
+            <li>Product</li>
+            <li>DarkEden</li>
+            <li>Softon</li>
+          </ul>
+        </nav>
       </header>
+      <main className="hero">
+      <video className='video-bg' autoPlay loop muted>
+            <source src={video} type='video/mp4' />
+        </video>
+        <div className="hero-content">
+          <p>Fiber is a versatile material that plays a crucial role in various industries, from textiles to advanced technology</p>
+          <p>DarkEden Classic Open Prayer Day 1</p>
+          <h2>sample</h2>
+        </div>
+      </main>
     </div>
   );
 }
